abb164b6d98fb11884fb5a873da1be8db9dd7223
Alex Brown <abrown@mta-trader.com>
2022-02-16 15:42:06 -0800

    A little build-out

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,10 @@
+import { Link } from 'react-router-dom'
+
 const Footer = props => {
+    // return <div className="Footer">
+    //     <span style={{ color: "dimgray" }}>Mortgage Trading Analytics, LLC</span>
+    //     <span style={{ float: "right" }}><Link to="https://tools.mta-trader.com/contact">Contact</Link></span>
+    // </div>
     return <></>
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,11 @@
+import { Navbar, NavbarBrand, Nav, NavItem, Collapse, NavbarToggler, UncontrolledDropdown, DropdownMenu, DropdownItem } from 'reactstrap'
+
 const NavBar = props => {
-    return <></>
+    return <Navbar light expand="md" className="NavBar-nav">
+        <NavbarBrand href="/">
+            <img src='./../images/logo.png' style={{maxHeight: "5rem"}} />
+        </NavbarBrand>
+    </Navbar>
 }
 
 export default NavBar
