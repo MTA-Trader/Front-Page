7188a142e15f6c81fe8cd954d2c6f65197b93b2c
Alex Brown <abrown@mta-trader.com>
2022-02-17 16:20:33 -0800

    Work With MTA section. Fix unable to scale to small sizes.

--- src/components/PageContainer.js
+++
@@ -1,9 +0,0 @@
-const PageContainer = props => {
-    return <div style={{ display: "flex", justifyContent: "center", margin: "0 2rem 0 2rem" }}>
-        <div style={{ maxWidth: "85rem" }}>
-            {props.children}
-        </div>
-    </div>
-}
-
-export default PageContainer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import PageContainer from "../components/PageContainer"
+import { Container } from "reactstrap"
+
 import Footer from "../components/Footer"
 
 const HomePage = props => {
-    return <PageContainer>
+    return <Container style={{ maxWidth: "85rem" }}>
         <img className="full-width" style={{ objectFit: "cover", height: "30rem" }} src="../../images/transamerica-pyramid.jpg" alt="Transamerica Pyramid" />
-        <div style={{ margin: "1rem 0 1rem 0" }}>
+        <div style={{ margin: "2rem 0 2rem 0" }}>
             <h4 style={{ fontWeight: 600, textAlign: "center" }}>Who We Are</h4>
             Mortgage Trading Analytics, LLC (MTA) is a capital markets optimization shop, specializing in risk management and trading arbitrage with a focus on technology. Cultivating and creating various trading opportunities as well as analyzing multiple secondary marketing sales options allows MTA to bridge the gap between where you are and where you want to be.
             <br /><br />
@@ -12,11 +13,10 @@ const HomePage = props => {
             <br /><br />
             In addition to risk management and trading arbitrage, MTA is well-versed in strategic consulting services, specializing in problem detection and resolution. Previous assignments run the gamut: operational tuning, system build-outs, code reconciliation, financial modeling, forecasting, pipeline metrics, custom analytics, best-ex sales solutions, trade improvement, department creation.
         </div>
-        {/* <img className="full-width" style={{ objectFit: "cover", height: "30rem" }} src="../../images/golden-gate-bridge.jpg" alt="SF Golden Gate Bridge" /> */}
         <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
-            <img style={{ flexBasis: "auto", flexGrow: 1, maxWidth: "40rem" }} src="../../images/gains.png" alt="Gains" />
+            <img style={{ flexBasis: "auto", flexGrow: 1, minWidth: 0, maxWidth: "40rem" }} src="../../images/gains.png" alt="Gains" />
             <div style={{ flexBasis: "20rem", flexGrow: 2, padding: "1rem" }}>
-                <h4 style={{ fontWeight: 600, textAlign: "center", marginTop: "5rem" }}>What We Do</h4>
+                <h4 style={{ fontWeight: 600, textAlign: "center"}}>What We Do</h4>
                 <h5>Proprietary Trading Models &#38; Arbitrage System</h5>
                 Using exciting new technologies and proprietary algorithms, MTA has a unique approach to helping improve your pipeline hedge. G-Fee analysis allows you to know when a trade is cheap, when it's not, and by exactly how much. Investor-pricing strength-indicators let you see exactly how good a price you're getting, with whom, when, and how it sits relative to the GSEs. New processes and specialized securities allow you to expand your hedging options, generating new revenue streams and improving the current ones.
                 <br /><br />
@@ -36,8 +36,22 @@ const HomePage = props => {
                 Don't have a lock desk? Need your Secondary Marketing Department reorganized? Does Capital Markets need a tear-down and rebuild? MTA has done all of that. Or perhaps it's time to shrink them down and outsource a good bit of the process. MTA can do that too, helping you downsize or offload various capital markets processes... or just let us handle it for you.
             </div>
         </div>
+        <div style={{ margin: "2rem 0 2rem 0" }}>
+            <h4 style={{ fontWeight: 600, textAlign: "center" }}>Work With MTA</h4>
+            <h5>Vendor Services</h5>
+            Sign-up for a full-service vendor relationship with MTA and let us handle as much or as little as you need. Capital Markets tuning, bringing your trading in-house, managing your trading, margin controls, pricing restructure, arbitrage trades... we'll work with your current processes, vendors, and systems, improve upon them, and make your Secondary profitability our primary concern.
+            <br /><br />
+            <h5>Consulting Services</h5>
+            Have a specific issue that you can't solve? Need some additional, specialized minds to work out a problem? Not sure you're ready for a full-time vendor relationship? Let MTA offer solutions.
+            <br /><br />
+            <h5>Subscription Services</h5>
+            We offer access to unique and specialized tools to optimize your trades, enhance your analytics, and improve your bottom line.
+            No time wasted retrieving and uploading data. MTA pulls, uploads, and disseminates everything to you and for you.
+            All results are easily extractable in a user-friendly CSV format.
+        </div>
+        <img className="full-width" style={{ objectFit: "cover", height: "30rem" }} src="../../images/golden-gate-bridge.jpg" alt="SF Golden Gate Bridge" />
         <Footer />
-    </PageContainer>
+    </Container>
 }
 
 export default HomePage
